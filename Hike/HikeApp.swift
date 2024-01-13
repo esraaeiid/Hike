11ca36794050e4f24bac049520a7edc1c7774ed9
Esraa <esraa.eid.metwally@gmail.com>
2024-01-13 22:55:57 +0200

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Esraa Eid on 13/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
